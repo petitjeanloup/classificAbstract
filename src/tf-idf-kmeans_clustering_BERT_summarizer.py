aae4cb90d0c422f26c47829bca432aa897360d4e
Loup PETITJEAN <petitjea@SUPAGRO.INRA.LAN>
2025-01-09 11:41:59 +0100

    modifs obs 1000 premiers termes

--- src/tf-idf-kmeans_clustering_BERT_summarizer.py
+++ src/tf-idf-kmeans_clustering_BERT_summarizer.py
@@ -20,7 +20,7 @@ X = vectorizer.fit_transform(abstracts)
 # Déterminer le nombre optimal de clusters avec la méthode du coude
 
 inertia = []
-k_values = range(2, 11)
+k_values = range(2, 30)
 
 for k in k_values:
     kmeans = KMeans(n_clusters=k, random_state=42)
@@ -37,7 +37,7 @@ plt.ylabel("Inertie")
 plt.show()
 
 
-n_clusters = 6
+n_clusters = 10
 kmeans = KMeans(n_clusters = n_clusters, random_state=42)
 clusters = kmeans.fit_predict(X)
 
@@ -53,6 +53,25 @@ top_terms_per_cluster = []
 centroids = kmeans.cluster_centers_
 terms = vectorizer.get_feature_names_out()
 
+
+
+
+term_frequencies = X.toarray().sum(axis=0)  # Fréquence totale des mots dans le corpus
+
+# Créer un DataFrame avec les résultats
+tfidf_stats = pd.DataFrame({
+    'Word': terms,
+    'Frequency': term_frequencies
+})
+
+# Trier par fréquence décroissante
+tfidf_stats = tfidf_stats.sort_values(by="Frequency", ascending=False).reset_index(drop=True)
+N = 20
+print("Frequence terms, ordre etc :")
+print(tfidf_stats.head(N), "\n")
+tfidf_stats.to_csv("./data/1000_terms.csv", index = False)
+
+
 for i in range(n_clusters):
 
     top_terms = [terms[ind] for ind in centroids[i].argsort()[-15:]]
@@ -62,3 +81,26 @@ for i in range(n_clusters):
 
 for i, top_term_in_cluster in enumerate(top_terms_per_cluster):
   print(f"Top terms in cluster {i} : ",top_term_in_cluster)
+
+
+data["int_Equipe"] = data["Equipe"].astype("category")
+data["int_Equipe"] = [data["int_Equipe"].cat.categories.get_loc(equipe) for equipe in data["int_Equipe"]]
+corr = data['int_Equipe'].corr(data['Cluster'])
+print("Correlation Cluster/Equipe : ", corr)
+
+total = 0
+for cluster in range(n_clusters):
+
+  data_c = data[data["Cluster"] == cluster]
+  total_cluster = 0
+
+  for equipe in data["Equipe"].unique():
+
+    data_c_equipe = data_c[data_c["Equipe"] == equipe]
+    print(f"In cluster {cluster} and equipe {equipe} : ", data_c_equipe.shape[0])
+    total += data_c_equipe.shape[0]
+    total_cluster += data_c_equipe.shape[0]
+
+  print(f"Total in cluster {cluster} : ", total_cluster)
+
+print(total)
